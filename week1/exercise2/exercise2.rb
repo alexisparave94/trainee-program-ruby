38351f5276cf0e416dd2f3225c8a8b18fed3d974
Alexis Parave <aparavev@gmail.com>
2022-10-05 23:25:25 -0500

    Add comments to exercise 2

--- week1/exercise2/exercise2.rb
+++ week1/exercise2/exercise2.rb
@@ -1,7 +1,12 @@
+# Call class Array
 class Array
+  # Add method to generate a histogram
   def to_histogram
     hash = {}
+    # Loop to count how many times an element of an array is repeated
     each do |e|
+      # If it is the first time that find an element the initial value will be 1 with a key equal to the element
+      # If the element exits as a key the value will increase in 1
       hash[e] ? hash[e] += 1 : hash[e] = 1
     end
     puts hash
