229ff594cf1763182ae7a17fdead05a85740ca82
Alexis Parave <aparavev@gmail.com>
2022-10-05 23:17:26 -0500

    Add comments to exercise 1

--- week1/exercise1/exercise1.rb
+++ week1/exercise1/exercise1.rb
@@ -1,24 +1,31 @@
+# Method to decode str in its correspondent position of the series: "a,b,c,d,..., z,aa,ab,ac,...,zy,zz,aaa,aab,..."
 def decode_string(str)
+  # Generate the alphabet: abc = [a, b, c, ..., z]
   abc = (97..122).map(&:chr)
   arr = str.split('').reverse
   num = 0
+  # This loop transform the letter to its correspondent position in the series
   arr.each.with_index do |e, i|
     num += (26**i) * (abc.index(e) + 1)
   end
   num
 end
 
-def generate_secuence(init_str, fin_str)
-  secuence = []
+# Method to generate the series
+def generate_series(init_str, fin_str)
+  series = []
+  # Position where the series starts
   a1 = decode_string(init_str)
+  # Position where the series finishes
   an = decode_string(fin_str)
   i = a1
+  # Loop to traverse all terms from a1 to an positions
   while i < an + 1
-    secuence.push(init_str)
+    series.push(init_str)
     init_str = init_str.succ
     i += 1
   end
-  puts secuence.join(',')
+  puts series.join(',')
 end
 
-generate_secuence("df", "aad")
+generate_series("xe", "aad")
