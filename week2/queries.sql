7749cd67e50cc1ba2da51f03bebc4771c3a607b3
Alexis Parave <aparavev@gmail.com>
2022-10-14 15:19:16 -0500

    Add query to select all the products with the profits

--- week2/queries.sql
+++ week2/queries.sql
@@ -6,3 +6,10 @@ SELECT * FROM customers;
 
 -- SELECT ALL ORDER LINES FROM AN ORDER
 SELECT * FROM order_lines WHERE order_id = X;
+
+-- SELECT ALL THE PRODUCTS WITH THE PROFITS
+SELECT sku, name, SUM(order_lines.total) AS "Total profits" FROM orders
+INNER JOIN order_lines ON orders.id = order_lines.order_id
+INNER JOIN products ON products.id = order_lines.product_id
+WHERE status = 'completed'
+GROUP BY sku, name;
