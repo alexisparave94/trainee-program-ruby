4209f36cf3901b8a7cc62dc16da40d6b498e296f
Alexis Parave <aparavev@gmail.com>
2022-10-14 19:58:17 -0500

    Add class vehicle with its instance variables and accesors

---
+++ week2/exercise2/lib/vehicle.rb
@@ -0,0 +1,10 @@
+class Vehicle
+  attr_accessor :wheels_number, :color, :brand, :price
+
+  def initialize(wheels_number, color, brand, price)
+    @wheels_number = wheels_number
+    @color = color
+    @brand = brand
+    @price = price
+  end
+end
