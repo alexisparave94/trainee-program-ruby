01ae93086bd057337975e39feef1a682113ed5cd
Alexis Parave <aparavev@gmail.com>
2022-10-14 19:06:00 -0500

    Add query to select all the customers who bought an X product

--- week2/queries.sql
+++ week2/queries.sql
@@ -26,3 +26,11 @@ WHERE product_id = X AND status = 'completed';
 
 -- UPDATE A PRODUCT PRICE BY 1.5 TIMES ITS VALUE
 UPDATE products SET price = price * 1.5 WHERE id = X;
+
+-- SELECT ALL THE CUSTOMERS WHO BOUGHT AN X PRODUCT
+SELECT first_name, last_name, product_id, products.name FROM customers
+INNER JOIN orders ON customers.id = orders.customer_id
+INNER JOIN order_lines ON orders.id = order_lines.order_id
+INNER JOIN products ON products.id = order_lines.product_id
+WHERE product_id = X AND status = 'completed'
+GROUP BY first_name, last_name, product_id, products.name;
