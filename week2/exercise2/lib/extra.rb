eb4f5d9a276dd3867064305bf699ac2893d8aa25
Alexis Parave <aparavev@gmail.com>
2022-10-14 20:30:37 -0500

    Add class extra with instance variables and constans

---
+++ week2/exercise2/lib/extra.rb
@@ -0,0 +1,21 @@
+class Extra
+  PRICES_EXTRAS = { 'Radio' => 500.0, 'AC' => 1000.0, 'Sunroof' => 2000.0, 'Leather Seats' => 1000.0, 'Power Windows' => 1000.0 }.freeze
+  attr_reader :extras_with_prices, :vehicle
+
+  def initialize(vehicle, extras = [])
+    @extras_with_prices = get_extras_with_prices(extras)
+    @vehicle = vehicle
+  end
+
+  def self.get_prices_extras
+    PRICES_EXTRAS
+  end
+
+  private
+
+  def get_extras_with_prices(extras)
+    extras_with_prices = {}
+    extras.each { |extra| extras_with_prices[extra] = PRICES_EXTRAS[extra] }
+    extras_with_prices
+  end
+end
