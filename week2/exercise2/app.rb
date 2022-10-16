ee4d9c643ad2140b26757993c1ff6efe1a2f9740
Alexis Parave <aparavev@gmail.com>
2022-10-15 21:38:30 -0500

    Add flow to class app

--- week2/exercise2/app.rb
+++ week2/exercise2/app.rb
@@ -7,6 +7,25 @@ class App
 
   def start
     populate_store
+    puts 'Welcome to Store App'
+    option = ''
+    until option == 5
+      puts 'Choose the option you want to do'
+      puts '1. List vehicles'
+      puts '2. Add a vehicle'
+      puts '3. Remove a vehicle'
+      puts '4. Generate quote'
+      puts '5. Exit'
+      print 'Enter the number of the option > '
+      option = gets.chomp.to_i
+      case option
+      when 1 then puts 'list vehicle'
+      when 2 then puts 'add_vehicle'
+      when 3 then puts 'remove vehicle'
+      when 4 then puts 'generate quote'
+      end
+    end
+    puts 'Thanks for use app'
   end
 
   private
