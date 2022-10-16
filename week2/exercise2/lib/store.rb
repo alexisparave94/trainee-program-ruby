efafe5bd8d95c210998963c40b6f02112b56009b
Alexis Parave <aparavev@gmail.com>
2022-10-15 21:06:45 -0500

    Add method list_vehicles to store class

--- week2/exercise2/lib/store.rb
+++ week2/exercise2/lib/store.rb
@@ -4,4 +4,13 @@ class Store
   def initialize
     @vehicles = []
   end
+
+  def list_vehicles
+    vehicles.each.with_index do |store_vehicle, index|
+      print "#{index + 1}. ID: #{store_vehicle['id']}, "
+      print "#{store_vehicle['obj_vehicle'].brand} #{store_vehicle['obj_vehicle'].color}, "
+      print "price: $#{store_vehicle['obj_vehicle'].price}, "
+      print "type: #{store_vehicle['type']}\n"
+    end
+  end
 end
