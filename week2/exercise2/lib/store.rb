5bb5a53b180ea94b1cbf20ea729a49d2cb9dad03
Alexis Parave <aparavev@gmail.com>
2022-10-15 21:09:13 -0500

    Add method remove_vehicle to remove a vehicle from vehicles intance variable of class store

--- week2/exercise2/lib/store.rb
+++ week2/exercise2/lib/store.rb
@@ -13,4 +13,15 @@ class Store
       print "type: #{store_vehicle['type']}\n"
     end
   end
+
+  def add_vehicle(vehicle)
+    id = SecureRandom.uuid
+    type = vehicle.wheels_number == 4 ? 'car' : 'truck'
+    store_vehicle = { 'id' => id, 'obj_vehicle' => vehicle, 'type' => type }
+    vehicles.push(store_vehicle)
+  end
+
+  def remove_vehicle(id)
+    vehicles.reject! { |store_vehicle| store_vehicle['id'] == id }
+  end
 end
