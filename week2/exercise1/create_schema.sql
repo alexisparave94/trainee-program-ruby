e41485f74b6bb5f40935868af5406ce58c5a97a2
Alexis Parave <aparavev@gmail.com>
2022-10-16 20:34:58 -0500

    Add comments to exercise1

--- week2/exercise1/create_schema.sql
+++ week2/exercise1/create_schema.sql
@@ -1,9 +1,11 @@
 BEGIN;
+-- Clean database
 DROP TABLE IF EXISTS order_lines;
 DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS customers;
 
+-- Create customers table
 CREATE TABLE customers (
   id serial PRIMARY KEY,
   first_name varchar NOT NULL,
@@ -12,6 +14,7 @@ CREATE TABLE customers (
   phone varchar NOT NULL
 );
 
+-- Create products table
 CREATE TABLE products (
   id serial PRIMARY KEY,
   sku varchar UNIQUE NOT NULL,
@@ -21,6 +24,7 @@ CREATE TABLE products (
   stock integer DEFAULT 0 CHECK (stock >= 0)
 );
 
+-- Create orders table
 CREATE TABLE orders (
   id serial PRIMARY KEY,
   date date,
@@ -29,6 +33,7 @@ CREATE TABLE orders (
   customer_id INTEGER NOT NULL REFERENCES customers(id)
 );
 
+-- Create order_lines table
 CREATE TABLE order_lines (
   order_id integer NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
   product_id integer NOT NULL REFERENCES products(id),
@@ -40,8 +45,8 @@ CREATE TABLE order_lines (
 
 COMMIT;
 
-
 BEGIN;
+--Create function to update the total of an order
 CREATE OR REPLACE function SP_Update_total() returns Trigger
 AS
 $$
@@ -52,6 +57,7 @@ $$
 $$
 LANGUAGE plpgsql;
 
+--Create the trigger to execute the storage procedure SP_Update_total each time a row is inserted in order_lines table
 CREATE Trigger TR_Insert_order_line AFTER INSERT ON order_lines
 for each row
 EXECUTE PROCEDURE SP_Update_total();
