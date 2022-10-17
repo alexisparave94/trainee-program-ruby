d13d3b75f6cd209a7143f491a43f8570a44e55cb
Alexis Parave <aparavev@gmail.com>
2022-10-16 23:01:52 -0500

    Add comments to exercise2 of week 2

--- week2/exercise2/lib/extra.rb
+++ week2/exercise2/lib/extra.rb
@@ -1,7 +1,10 @@
+# Class Extra to represent additional features of a vehicle
 class Extra
+  # Constants
   PRICES_EXTRAS = { 'Radio' => 500.0, 'AC' => 1000.0, 'Sunroof' => 2000.0, 'Leather Seats' => 1000.0, 'Power Windows' => 1000.0 }.freeze
   EXTRAS = ['Radio', 'AC', 'Sunroof', 'Leather Seats', 'Power Windows'].freeze
 
+  # Attributes
   attr_reader :extras_with_prices, :vehicle
 
   def initialize(vehicle, extras = [])
@@ -9,18 +12,21 @@ class Extra
     @vehicle = vehicle
   end
 
+  # Method to show the additional features(extras) that a vehicle has
   def list_vehicle_extras
     extras_with_prices.each do |extra, price|
       puts "\t#{extra}: ($#{price})\n\n"
     end
   end
 
+  # Method to import extras to a vehicle
   def import_extras
     vehicle.extras = extras_with_prices
   end
 
   private
 
+  # Method to set what extras a vehicle has
   def get_extras_with_prices(extras)
     extras_with_prices = {}
     extras.each { |extra| extras_with_prices[extra] = PRICES_EXTRAS[extra] }
